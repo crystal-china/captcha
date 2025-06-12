ada21cb170ed5975799159f68f12657b4a3d927b
Billy.Zheng <vil963@gmail.com>
2025-06-13 01:10:56 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # captcha
 
-TODO: Write a description here
+Crystal library that generates image CAPTCHAs.
+
+![](images/example.webp)
+
+All credits goes to [the example code in crystal-vips](https://github.com/naqvis/crystal-vips/blob/0f4d3914985865a020168b0f48ece07416eeb459/example/captcha_generator.cr) by [@naqvis](https://github.com/naqvis).
 
 ## Installation
 
@@ -9,7 +13,7 @@ TODO: Write a description here
    ```yaml
    dependencies:
      captcha:
-       github: zw963/captcha
+       github: crystal-china/captcha
    ```
 
 2. Run `shards install`
@@ -18,6 +22,20 @@ TODO: Write a description here
 
 ```crystal
 require "captcha"
+
+captcha = Captcha.new(length: 6)
+
+# Open a.html to check if it work.
+captcha.write_html_file("a.html")
+
+# Return underlying image data as Bytes
+captcha.buffer # Bytes[82, 73, 70, 70, 16 ... ]
+
+# A string present the html <image> tag which enbed the image in, use it directly in your page.
+captcha.image_tag # <img src="data:image/webp;base64,BASE64_ENCODED_IMAGE_DATA" />
+
+# return the underlying text
+captcha.text # nh8S8G
 ```
 
 TODO: Write usage instructions here
@@ -28,7 +46,7 @@ TODO: Write development instructions here
 
 ## Contributing
 
-1. Fork it (<https://github.com/zw963/captcha/fork>)
+1. Fork it (<https://github.com/crystal-china/captcha/fork>)
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
@@ -36,4 +54,5 @@ TODO: Write development instructions here
 
 ## Contributors
 
-- [Billy.Zheng](https://github.com/zw963) - creator and maintainer
+- [Ali Naqvi](https://github.com/naqvis) creator and maintainer
+- [Billy.Zheng](https://github.com/zw963) - maintainer
--- src/captcha.cr
+++ src/captcha.cr
@@ -1,6 +1,142 @@
-require "./captcha/**"
-# TODO: Write documentation for `Captcha`
-module Captcha
+# Stolen from https://github.com/naqvis/crystal-vips/blob/0f4d3914985865a020168b0f48ece07416eeb459/example/captcha_generator.cr
 
-  # TODO: Put your code here
+require "vips"
+require "base58"
+
+# Captcha generator
+# Reference: https://github.com/libvips/libvips/issues/898
+
+class Captcha
+  getter! buffer : Bytes
+  getter! image_tag : String
+  getter! text : String
+
+  def initialize(length : Int32 = 4, text : String? = nil, format : String = "webp")
+    text = Random.base58(length) if text.nil?
+    @text = text
+
+    text_layer = Vips::Image.black 1, 1
+    x_position = 0
+
+    text.each_char do |c|
+      letter, _ = Vips::Image.text(c.to_s, dpi: 600)
+
+      image = letter.gravity(
+        direction: Vips::Enums::CompassDirection::Centre,
+        width: letter.width + 50,
+        height: letter.height + 50
+      )
+
+      image = scale_and_rotate(image)
+      image = wobble(image)
+      image = random_color(image)
+      image = nine_bit_srgb(image)
+      image = position_to(image: image, x: x_position, y: 0)
+
+      text_layer += image
+      text_layer = text_layer.cast(Vips::Enums::BandFormat::Uchar)
+
+      x_position += letter.width
+    end
+
+    # remove any unused edges
+    text_layer = text_layer.crop(*text_layer.find_trim(background: 0))
+
+    # make an alpha for the text layer: just a mono version of the image, but scaled
+    # up so letters themeselves are not transparent
+    alpha = (text_layer.colourspace(space: Vips::Enums::Interpretation::Bw) * 3)
+      .cast(format: Vips::Enums::BandFormat::Uchar)
+    text_layer = text_layer.bandjoin(alpha)
+
+    # make a white background with random speckles
+    speckles = Vips::Image.gaussnoise(
+      width: text_layer.width,
+      height: text_layer.height,
+      mean: 400,
+      sigma: 200,
+    )
+    background = (1..3).reduce(speckles) do |a, _|
+      a.bandjoin(speckles).copy(
+        interpretation: Vips::Enums::Interpretation::Srgb
+      ).cast(format: Vips::Enums::BandFormat::Uchar)
+    end
+
+    # composite the text over the background
+    final = background.composite(
+      image: text_layer,
+      mode: Vips::Enums::BlendMode::Over
+    )
+
+    @buffer = final.write_to_buffer("%.#{format}")
+
+    @image_tag = <<-HEREDOC
+<img src="data:image/#{format};base64,#{Base64.encode(buffer)}"/>
+HEREDOC
+  end
+
+  def write_html_file(name)
+    html = <<-HEREDOC
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Base64 Image Demo</title>
+  </head>
+
+  <body>
+    <h1>显示 captcha 图片</h1>
+    #{@image_tag}
+  </body>
+</html>
+HEREDOC
+
+    File.write(name, html)
+  end
+
+  # position at our write position in the image
+  private def position_to(image, x, y)
+    image.embed(x, y, image.width + x, image.height + y)
+  end
+
+  # random scale and rotate
+  private def scale_and_rotate(image)
+    image.similarity(
+      scale: Random.rand(0.2) + 0.8,
+      angle: Random.rand(40) - 20
+    )
+  end
+
+  # random color
+  private def random_color(image)
+    color = (1..3).map { Random.rand(255) }
+    image.ifthenelse(color, 0, true)
+  end
+
+  # tag as 9-bit srgb
+  private def nine_bit_srgb(image)
+    image.copy(
+      interpretation: Vips::Enums::Interpretation::Srgb
+    ).cast(Vips::Enums::BandFormat::Uchar)
+  end
+
+  # random wobble
+  private def wobble(image)
+    # a warp image is a 2D grid containing the new coordinates of each pixel with
+    # the new x in band 0 and the new y in band 1
+    #
+    # you can also use a complex image
+    #
+    # start from a low-res XY image and distort it
+
+    xy = Vips::Image.xyz(image.width // 20, image.height // 20)
+    x_distort = Vips::Image.gaussnoise(xy.width, xy.height)
+    y_distort = Vips::Image.gaussnoise(xy.width, xy.height)
+    xy += (x_distort.bandjoin(y_distort) / 150)
+    xy = xy.resize(20)
+    xy *= 20
+
+    # apply the warp
+    image.mapim(xy)
+  end
 end
